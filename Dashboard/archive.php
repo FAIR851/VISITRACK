d530e1fe7c062a8562706a2aa2dbbc99149d9fe5
FAIR851 <unfairness77@gmail.com>
2025-05-18 14:11:43 +0800

    Update archive.php

--- Dashboard/archive.php
+++ Dashboard/archive.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['email'])) {
 $email = $_SESSION['email'];
 
 // Search logic
-// Search logic
+
 $search = '';
 if (isset($_GET['search']) && !empty($_GET['search'])) {
     $search = mysqli_real_escape_string($conn, $_GET['search']);
@@ -26,7 +26,7 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
 }
 
 // Pagination logic
-$records_per_page = 13;
+$records_per_page = 15;
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $page = ($page < 1) ? 1 : $page; 
 $offset = ($page - 1) * $records_per_page;
@@ -50,35 +50,50 @@ $result = mysqli_query($conn, $query);
     <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
+    <div class="background">
+        <img src="../Images/ntcbg.jpg" alt="NTC Logo" class="bj1">
+    </div>
 <nav id="sidebar">
-        <ul>
-            <li><div class="logo-container">
-            <img src="../Images/logo.png" alt="NTC Logo" class="logo">
+    <ul>
+        <li>
+            <div class="logo-container">
+                <img src="../Images/logo.png" alt="NTC Logo" class="logo">
             </div>
-                <button onclick="toggleSidebar()" id="toggle-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg>
-                </button>
-            </li>
-            <li>
-                <a href="dashboard.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M560-564v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-600q-38 0-73 9.5T560-564Zm0 220v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-380q-38 0-73 9t-67 27Zm0-110v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-490q-38 0-73 9.5T560-454ZM260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z"/></svg>                    <span>Logbook</span>
-                </a>
-            </li>
-            <li  class="active">>
-                <a href="archive.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m480-240 160-160-56-56-64 64v-168h-80v168l-64-64-56 56 160 160ZM200-640v440h560v-440H200Zm0 520q-33 0-56.5-23.5T120-200v-499q0-14 4.5-27t13.5-24l50-61q11-14 27.5-21.5T250-840h460q18 0 34.5 7.5T772-811l50 61q9 11 13.5 24t4.5 27v499q0 33-23.5 56.5T760-120H200Zm16-600h528l-34-40H250l-34 40Zm264 300Z"/></svg>
-                    <span>Archive</span>
-                </a>
-            </li>
-            <li>
-                <a href="../logout.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
-                    <span style="color: rgb(193, 0, 0)">Log Out</span>
-                </a>
-            </li>
-            
-        </ul>
-    </nav>
+            <button onclick="toggleSidebar()" id="toggle-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/>
+                </svg>
+            </button>
+        </li>
+
+        <li>
+            <a href="dashboard.php" class="<?= basename($_SERVER['PHP_SELF']) == 'dashboard.php' ? 'active' : '' ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="M560-564v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-600q-38 0-73 9.5T560-564Zm0 220v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-380q-38 0-73 9t-67 27Zm0-110v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-490q-38 0-73 9.5T560-454ZM260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z"/>
+                </svg>
+                <span>Logbook</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="archive.php" class="<?= basename($_SERVER['PHP_SELF']) == 'archive.php' ? 'active' : '' ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="m480-240 160-160-56-56-64 64v-168h-80v168l-64-64-56 56 160 160ZM200-640v440h560v-440H200Zm0 520q-33 0-56.5-23.5T120-200v-499q0-14 4.5-27t13.5-24l50-61q11-14 27.5-21.5T250-840h460q18 0 34.5 7.5T772-811l50 61q9 11 13.5 24t4.5 27v499q0 33-23.5 56.5T760-120H200Zm16-600h528l-34-40H250l-34 40Zm264 300Z"/>
+                </svg>
+                <span>Archive</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="../logout.php" class="<?= basename($_SERVER['PHP_SELF']) == 'logout.php' ? 'active' : '' ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
+                </svg>
+                <span style="color: rgb(193, 0, 0)">Log Out</span>
+            </a>
+        </li>
+    </ul>
+</nav>
 
 <main>
     <div id="main-content">
@@ -87,12 +102,13 @@ $result = mysqli_query($conn, $query);
         <div class="search-sort-container">
             <form method="GET" action="archive.php" class="search-form">
                 <input type="text" name="search" placeholder="Search visitors..." value="<?= htmlspecialchars($search); ?>">
-                <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" fill="#5f6368">
-                        <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
+                <button type="submit" aria-label="Search">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="#5f6368" viewBox="0 0 24 24">
+                        <path d="M15.5 14h-.79l-.28-.27a6.471 6.471 0 001.48-5.34C15.16 5.61 12.55 3 9.5 3S3.84 5.61 3.84 8.39c0 2.78 2.61 5.39 5.66 5.39a6.471 6.471 0 005.34-1.48l.27.28v.79l4.25 4.25 1.49-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
                     </svg>
                 </button>
             </form>
+
             <a href="../index.php" class="register-btn">Register</a>
 
             <form method="post" action="../export.php">
